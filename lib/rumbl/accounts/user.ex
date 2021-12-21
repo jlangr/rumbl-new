f2e55d13895682f94d1f9833835e6de41603fecb
Jeff L <jeff@langrsoft.com>
2021-12-20 21:30:18 -0700

    videos

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -17,6 +17,7 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username, message: "That username has already been taken")
   end
 
   def registration_changeset(user, params) do
